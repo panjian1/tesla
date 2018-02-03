0063fa304c9170ce9e7f85f9f5cf646a2ddd3e8c
shiming.liu <shiming.liu@dianrong.com>
2018-02-03 22:23:30 +0800

    update

--- tesla-authz/src/main/java/io/github/tesla/authz/AuthzConfig.java
+++ tesla-authz/src/main/java/io/github/tesla/authz/AuthzConfig.java
@@ -28,7 +28,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
