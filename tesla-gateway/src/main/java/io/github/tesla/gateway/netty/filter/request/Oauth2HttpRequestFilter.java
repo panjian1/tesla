767be256388fa1ea4d8ab9dfae18daceb390d040
shiming.liu <shiming.liu@dianrong.com>
2018-02-10 18:32:36 +0800

    add oauth2 resource

---
+++ tesla-gateway/src/main/java/io/github/tesla/gateway/netty/filter/request/Oauth2HttpRequestFilter.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.tesla.gateway.netty.filter.request;
+
+import org.apache.oltu.oauth2.common.message.types.ParameterStyle;
+import org.apache.oltu.oauth2.rs.request.OAuthAccessResourceRequest;
+
+import io.github.tesla.gateway.config.SpringContextHolder;
+import io.github.tesla.gateway.netty.servlet.NettyHttpServletRequest;
+import io.github.tesla.gateway.routerules.Oauth2TokenComponent;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+
+/**
+ * @author liushiming
+ * @version Oauth2HttpRequestFilter.java, v 0.0.1 2018年2月10日 下午6:12:20 liushiming
+ */
+public class Oauth2HttpRequestFilter extends HttpRequestFilter {
+
+
+  private final Oauth2TokenComponent oauth2TokenCache =
+      SpringContextHolder.getBean(Oauth2TokenComponent.class);
+
+  public static HttpRequestFilter newFilter() {
+    return new GrpcAdapterHttpRequestFilter();
+  }
+
+  @Override
+  public HttpResponse doFilter(HttpRequest originalRequest, HttpObject httpObject,
+      ChannelHandlerContext channelHandlerContext) {
+    if (httpObject instanceof HttpRequest) {
+      try {
+        HttpRequest httpRequest = (HttpRequest) httpObject;
+        NettyHttpServletRequest servletRequest =
+            new NettyHttpServletRequest(httpRequest, "/", channelHandlerContext);
+        OAuthAccessResourceRequest oauthRequest =
+            new OAuthAccessResourceRequest(servletRequest, ParameterStyle.QUERY);
+        String accessToken = oauthRequest.getAccessToken();
+        if (!oauth2TokenCache.checkAccessToken(accessToken)) {
+          return super.createResponse(HttpResponseStatus.FORBIDDEN, originalRequest);
+        }
+      } catch (Throwable e) {
+        return super.createResponse(HttpResponseStatus.FORBIDDEN, originalRequest);
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public int filterOrder() {
+    return 0;
+  }
+
+}
