6dd5ae3adcd793d1c7c95fc23c8ccb8997924c25
shiming.liu <shiming.liu@dianrong.com>
2018-03-26 10:34:08 +0800

    up

--- tesla-ops/src/main/resources/static/js/appjs/system/menu/add.js
+++ tesla-ops/src/main/resources/static/js/appjs/system/menu/add.js
@@ -1,7 +1,45 @@
-var prefix = "/sys/menu"
-$(function() {
-	validateRule();
-	// 打开图标列表
+$(document).ready(function(){
+	var prefix = "/sys/menu"
+	pageSetUp();
+    function goback(){
+	  loadURL("sys/menu", $('#content'));
+    }
+	var pagefunction = function() {
+		var $menuform = $("#menuform").validate({
+			rules : {
+				name : {
+					required : true
+				},
+				type : {
+					required : true
+				}
+			},
+			messages : {
+				name : {
+					required : "请输入菜单名"
+				},
+				type : {
+					required : "请选择菜单类型"
+				}
+			},
+			submitHandler : function(form) {
+				$(form).ajaxSubmit({
+					cache : true,
+					type : "POST",
+					url : prefix + "/save",
+					data : $('#menuform').serialize(),
+					async : false,
+					success : function() {
+						$("#menuform").addClass('submited');
+					}
+				});
+			},
+			errorPlacement : function(error, element) {
+				alert("test");
+				error.insertAfter(element.parent());
+			}
+		});
+	};
 	$("#ico-btn").click(function() {
 		layer.open({
 			type : 2,
@@ -12,54 +50,5 @@ $(function() {
 			}
 		});
 	});
+	loadScript("js/plugin/jquery-form/jquery-form.min.js", pagefunction);
 });
-$.validator.setDefaults({
-	submitHandler : function() {
-		submit01();
-	}
-});
-function submit01() {
-	$.ajax({
-		cache : true,
-		type : "POST",
-		url : prefix + "/save",
-		data : $('#signupForm').serialize(),
-		async : false,
-		error : function(request) {
-			laryer.alert("Connection error");
-		},
-		success : function(data) {
-			if (data.code == 0) {
-				parent.layer.msg("保存成功");
-				parent.reLoad();
-				var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
-				parent.layer.close(index);
-
-			} else {
-				layer.alert(data.msg)
-			}
-		}
-	});
-}
-
-function validateRule() {
-	var icon = "<i class='fa fa-times-circle'></i> ";
-	$("#signupForm").validate({
-		rules : {
-			name : {
-				required : true
-			},
-			type : {
-				required : true
-			}
-		},
-		messages : {
-			name : {
-				required : icon + "请输入菜单名"
-			},
-			type : {
-				required : icon + "请选择菜单类型"
-			}
-		}
-	})
-}
