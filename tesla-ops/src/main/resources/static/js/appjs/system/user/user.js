9d85a318f490a29b92b2e6c9c9fab458e98a74a6
shiming.liu <shiming.liu@dianrong.com>
2018-03-26 14:25:31 +0800

    up

--- tesla-ops/src/main/resources/static/js/appjs/system/user/user.js
+++ tesla-ops/src/main/resources/static/js/appjs/system/user/user.js
@@ -6,85 +6,83 @@ $(function() {
 });
 
 function load(deptId) {
-	$('#exampleTable')
-			.bootstrapTable(
+$('#userTable').bootstrapTable({
+			method : 'get',
+			url : prefix + "/list", 
+			iconSize : 'outline',
+			toolbar : '#exampleToolbar',
+			striped : true, 
+			dataType : "json", 
+			pagination : true, 
+			singleSelect : false,  
+			pageSize : 10,  
+			pageNumber : 1,  
+			showColumns : false,  
+			sidePagination : "server", 
+			queryParams : function(params) {
+				return {
+					limit : params.limit,
+					offset : params.offset,
+					name : $('#searchName').val(),
+					deptId : deptId
+				};
+			},
+			columns : [
 					{
-						method : 'get', // 服务器数据的请求方式 get or post
-						url : prefix + "/list", // 服务器数据的加载地址
-						iconSize : 'outline',
-						toolbar : '#exampleToolbar',
-						striped : true, // 设置为true会有隔行变色效果
-						dataType : "json", // 服务器返回的数据类型
-						pagination : true, // 设置为true会在底部显示分页条
-						singleSelect : false, // 设置为true将禁止多选
-						pageSize : 10, // 如果设置了分页，每页数据条数
-						pageNumber : 1, // 如果设置了分布，首页页码
-						showColumns : false, // 是否显示内容下拉框（选择显示的列）
-						sidePagination : "server", // 设置在哪里进行分页，可选值为"client" 或者
-						queryParams : function(params) {
-							return {
-								limit : params.limit,
-								offset : params.offset,
-								name : $('#searchName').val(),
-								deptId : deptId
-							};
-						},
-						columns : [
-								{
-									checkbox : true
-								},
-								{
-									field : 'userId', // 列字段名
-									title : '序号' // 列标题
-								},
-								{
-									field : 'name',
-									title : '姓名'
-								},
-								{
-									field : 'username',
-									title : '用户名'
-								},
-								{
-									field : 'email',
-									title : '邮箱'
-								},
-								{
-									field : 'status',
-									title : '状态',
-									align : 'center',
-									formatter : function(value, row, index) {
-										if (value == '0') {
-											return '<span class="label label-danger">禁用</span>';
-										} else if (value == '1') {
-											return '<span class="label label-primary">正常</span>';
-										}
-									}
-								},
-								{
-									title : '操作',
-									field : 'id',
-									align : 'center',
-									formatter : function(value, row, index) {
-										var e = '<a  class="btn btn-primary btn-sm '
-												+ s_edit_h
-												+ '" href="#" mce_href="#" title="编辑" onclick="edit(\''
-												+ row.userId
-												+ '\')"><i class="fa fa-edit "></i></a> ';
-										var d = '<a class="btn btn-warning btn-sm '
-												+ s_remove_h
-												+ '" href="#" title="删除"  mce_href="#" onclick="remove(\''
-												+ row.userId
-												+ '\')"><i class="fa fa-remove"></i></a> ';
-										var f = '<a class="btn btn-success btn-sm '
-												+ s_resetPwd_h
-												+ '" href="#" title="重置密码"  mce_href="#" onclick="resetPwd(\''
-												+ row.userId
-												+ '\')"><i class="fa fa-key"></i></a> ';
-										return e + d + f;
-									}
-								} ]
-					});
+						checkbox : true
+					},
+					{
+						field : 'userId', // 列字段名
+						title : '序号' // 列标题
+					},
+					{
+						field : 'name',
+						title : '姓名'
+					},
+					{
+						field : 'username',
+						title : '用户名'
+					},
+					{
+						field : 'email',
+						title : '邮箱'
+					},
+					{
+						field : 'status',
+						title : '状态',
+						align : 'center',
+						formatter : function(value, row, index) {
+							if (value == '0') {
+								return '<span class="label label-danger">禁用</span>';
+							} else if (value == '1') {
+								return '<span class="label label-primary">正常</span>';
+							}
+						}
+					},
+					{
+						title : '操作',
+						field : 'id',
+						align : 'center',
+						formatter : function(value, row, index) {
+							var e = '<a  class="btn btn-primary btn-sm '
+									+ s_edit_h
+									+ '" href="#" mce_href="#" title="编辑" onclick="edit(\''
+									+ row.userId
+									+ '\')"><i class="fa fa-edit "></i></a> ';
+							var d = '<a class="btn btn-warning btn-sm '
+									+ s_remove_h
+									+ '" href="#" title="删除"  mce_href="#" onclick="remove(\''
+									+ row.userId
+									+ '\')"><i class="fa fa-remove"></i></a> ';
+							var f = '<a class="btn btn-success btn-sm '
+									+ s_resetPwd_h
+									+ '" href="#" title="重置密码"  mce_href="#" onclick="resetPwd(\''
+									+ row.userId
+									+ '\')"><i class="fa fa-key"></i></a> ';
+							return e + d + f;
+						}
+					} ]
+		});
 }
 function reLoad() {
 	$('#exampleTable').bootstrapTable('refresh');
