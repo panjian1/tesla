17054440d748b51de58b6c39050b80e803540870
shiming.liu <shiming.liu@dianrong.com>
2018-03-28 11:01:50 +0800

    up

--- tesla-ops/src/main/resources/static/js/appjs/system/online/online.js
+++ tesla-ops/src/main/resources/static/js/appjs/system/online/online.js
@@ -5,18 +5,18 @@ $(function() {
 
 function load() {
   $('#onlineTable').bootstrapTable({
-    method: 'get', // 服务器数据的请求方式 get or post
-    url: prefix + "/list", // 服务器数据的加载地址
+    method: 'get',
+    url: prefix + "/list",
     iconSize: 'outline',
-    striped: true, // 设置为true会有隔行变色效果
-    dataType: "json", // 服务器返回的数据类型
-    pagination: true, // 设置为true会在底部显示分页条
-    singleSelect: false, // 设置为true将禁止多选
+    striped: true,
+    dataType: "json",
+    pagination: true,
+    singleSelect: false,
     pageSize: 5,
     pageList: [5],
-    pageNumber: 1, // 如果设置了分布，首页页码
-    showColumns: false, // 是否显示内容下拉框（选择显示的列）
-    sidePagination: "client", // 设置在哪里进行分页，可选值为"client" 或者
+    pageNumber: 1,
+    showColumns: false,
+    sidePagination: "client",
     queryParams: function(params) {
       return {
         limit: params.limit,
@@ -27,8 +27,8 @@ function load() {
     columns: [{
       checkbox: true
     }, {
-      field: 'id', // 列字段名
-      title: '序号' // 列标题
+      field: 'id',
+      title: '序号'
     }, {
       field: 'username',
       title: '用户名'
