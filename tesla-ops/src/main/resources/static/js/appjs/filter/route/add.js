03239e10595952e7279b30294aed1b53bdab1b73
shiming.liu <shiming.liu@dianrong.com>
2018-03-29 21:31:53 +0800

    up

--- tesla-ops/src/main/resources/static/js/appjs/filter/route/add.js
+++ tesla-ops/src/main/resources/static/js/appjs/filter/route/add.js
@@ -1,115 +1,132 @@
 $(document).ready(function() {
-	validateRule();
+  pageSetUp();
+  var pagefunction = function() {
+    loadScript("js/plugin/bootstrap-wizard/jquery.bootstrap.wizard.min.js", runBootstrapWizard);
+    function runBootstrapWizard() {
+      var $validator = $("#wizardFrom").validate({
+        rules: {
+          fromPath: {
+            required: true
+          },
+          serviceName: {
+            required: {
+              depends: function(value, element) {
+                var isRpc = $('#rpc').val();
+                return isRpc == 1;
+              }
+            }
+          },
+          methodName: {
+            required: {
+              depends: function(value, element) {
+                var isRpc = $('#rpc').val();
+                return isRpc == 1;
+              }
+            }
+          },
+          serviceGroup: {
+            required: {
+              depends: function(value, element) {
+                var isRpc = $('#rpc').val();
+                return isRpc == 1;
+              }
+            }
+          },
+          serviceVersion: {
+            required: {
+              depends: function(value, element) {
+                var isRpc = $('#rpc').val();
+                return isRpc == 1;
+              }
+            }
+          },
+          zipFile: {
+            required: {
+              depends: function(value, element) {
+                var isRpc = $('#rpc').val();
+                return isRpc == 1;
+              }
+            }
+          },
+          serviceFileName: {
+            required: {
+              depends: function(value, element) {
+                var isRpc = $('#rpc').val();
+                if (isRpc == 1) {
+                  return $('#zipFile').val() != null;
+                } else {
+                  return false;
+                }
+              }
+            }
+          }
+        },
+        messages: {
+          fromPath: {
+            required: "请输入路由路径！"
+          },
+          serviceName: {
+            required: "请输入服务名！"
+          },
+          methodName: {
+            required: "请输入方法名！"
+          },
+          serviceGroup: {
+            required: "请输入组别！"
+          },
+          serviceVersion: {
+            required: "请输入版本！"
+          },
+          zipFile: {
+            required: "请上传proto目录文件！"
+          },
+          serviceFileName: {
+            required: "上传proto目录文件，需要指定目录中的服务定义文件名！"
+          }
+        },
+        highlight: function(element) {
+          $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+        },
+        unhighlight: function(element) {
+          $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+          if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+          } else {
+            error.insertAfter(element);
+          }
+        }
+      });
+      $('#routeWizard').bootstrapWizard({
+        'tabClass': 'form-wizard',
+        'onNext': function(tab, navigation, index) {
+          var $valid = $("#wizardFrom").valid();
+          if (!$valid) {
+            $validator.focusInvalid();
+            return false;
+          } else {
+            $('#bootstrap-wizard-1').find('.form-wizard').children('li').eq(index - 1).addClass('complete');
+            $('#bootstrap-wizard-1').find('.form-wizard').children('li').eq(index - 1).find('.step').html('<i class="fa fa-check"></i>');
+          }
+        },
+        'onTabShow': function(tab, navigation, index) {
+          var $valid = $("#wizardFrom").valid();
+          if (!$valid) {
+            $validator.focusInvalid();
+            return false;
+          } else {
+            $('#bootstrap-wizard-1').find('.form-wizard').children('li').eq(index - 1).addClass('complete');
+            $('#bootstrap-wizard-1').find('.form-wizard').children('li').eq(index - 1).find('.step').html('<i class="fa fa-check"></i>');
+          }
+        },
+        'onFinish': function(tab, navigation, index) {
+          alert('test');
+        }
+      });
+    }
+  }
+  pagefunction();
 });
-$.validator.setDefaults({
-	submitHandler : function(form) {
-		save();
-	}
-});
-function save() {
-	$("#routeForm").ajaxSubmit({
-		type : "POST",
-		url : "/filter/route/save",
-		dataType : 'json',
-		error : function(request) {
-			parent.layer.alert("Connection error");
-		},
-		success : function(data) {
-			if (data.code == 0) {
-				parent.layer.msg("操作成功");
-				parent.reLoad();
-				var index = parent.layer.getFrameIndex(window.name);
-				parent.layer.close(index);
-
-			} else {
-				parent.layer.alert(data.msg)
-			}
-
-		}
-	});
-}
-function validateRule() {
-	var icon = "<i class='fa fa-times-circle'></i> ";
-	$("#routeForm").validate({
-		rules : {
-			fromPath : {
-				required : true
-			},
-			serviceName : {
-				required : {
-					depends : function(value, element) {
-						var isRpc = $('#rpc').val();
-						return isRpc == 1;
-					}
-				}
-			},
-			methodName : {
-				required : {
-					depends : function(value, element) {
-						var isRpc = $('#rpc').val();
-						return isRpc == 1;
-					}
-				}
-			},
-			serviceGroup : {
-				required : {
-					depends : function(value, element) {
-						var isRpc = $('#rpc').val();
-						return isRpc == 1;
-					}
-				}
-			},
-			serviceVersion : {
-				required : {
-					depends : function(value, element) {
-						var isRpc = $('#rpc').val();
-						return isRpc == 1;
-					}
-				}
-			},
-			zipFile : {
-				required : {
-					depends : function(value, element) {
-						var isRpc = $('#rpc').val();
-						return isRpc == 1;
-					}
-				}
-			},
-			serviceFileName : {
-				required : {
-					depends : function(value, element) {
-						var isRpc = $('#rpc').val();
-						if (isRpc == 1) {
-							return $('#zipFile').val() != null;
-						} else {
-							return false;
-						}
-					}
-				}
-			}
-		},
-		messages : {
-			fromPath : {
-				required : icon + "请输入路由路径！"
-			},
-			serviceName : {
-				required : "请输入服务名！"
-			},
-			methodName : {
-				required : "请输入方法名！"
-			},
-			serviceGroup : {
-				required : "请输入组别！"
-			},
-			serviceVersion : {
-				required : "请输入版本！"
-			},
-			zipFile : {
-				required : "请上传proto目录文件！"
-			},
-			serviceFileName : {
-				required : "上传proto目录文件，需要指定目录中的服务定义文件名！"
-			}
-		}
-	})
-}
