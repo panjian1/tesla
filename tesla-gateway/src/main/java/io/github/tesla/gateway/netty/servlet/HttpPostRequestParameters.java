ba878499ff692b04cfdba55a13ad89a9b6269617
shiming.liu <shiming.liu@dianrong.com>
2018-04-19 12:33:23 +0800

    up

--- tesla-gateway/src/main/java/io/github/tesla/gateway/netty/servlet/HttpPostRequestParameters.java
+++ tesla-gateway/src/main/java/io/github/tesla/gateway/netty/servlet/HttpPostRequestParameters.java
@@ -1,77 +1,67 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2014-2017 the original author or authors.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.github.tesla.gateway.netty.servlet;
-
-import com.google.common.collect.Maps;
-import io.netty.buffer.ByteBuf;
-import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.QueryStringDecoder;
 
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.Maps;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.QueryStringDecoder;
 
 /**
- * @author xiaoyu
+ * @author liushiming
+ * @version HttpPostRequestParameters.java, v 0.0.1 2018年4月19日 下午12:25:58 liushiming
  */
 public class HttpPostRequestParameters {
+  private final ByteBuf content;
 
-    private final HttpRequest               request;
-    private final ByteBuf                   content;
+  private final Map<String, List<String>> params = Maps.newHashMap();
+  private byte[] body;
+  private boolean _parsed = false;
 
-    private final Map<String, List<String>> params  = Maps.newHashMap();
-    private byte[]                          body;
-    private boolean                         _parsed = false;
+  public HttpPostRequestParameters(ByteBuf content) {
+    this.content = content;
+  }
 
-    public HttpPostRequestParameters(HttpRequest request, ByteBuf content) {
-        this.request = request;
-        this.content = content;
+  private byte[] getHttpRequestBodyAsBytes() {
+    if (body != null) {
+      return body;
     }
-
-    byte[] getHttpRequestBodyAsBytes() {
-        if (body != null) {
-            return body;
-        }
-        if (content == null) {
-            return null;
-        }
-
-        content.readerIndex(0);
-        int len = content.readableBytes();
-        body = new byte[len];
-        content.readBytes(body);
-
-        return body;
+    if (content == null) {
+      return null;
     }
+    content.readerIndex(0);
+    int len = content.readableBytes();
+    body = new byte[len];
+    content.readBytes(body);
+    return body;
+  }
 
-    Map<String, List<String>> getHttpRequestParameters() {
-        if (_parsed) {
-            return params;
-        }
-
-        byte[] body = getHttpRequestBodyAsBytes();
-        if (body == null) {
-            _parsed = true;
-            return params;
-        }
-
-        QueryStringDecoder decoder = new QueryStringDecoder("?" + new String(body));
-        params.putAll(decoder.parameters());
-        _parsed = true;
-        return params;
+  Map<String, List<String>> getHttpRequestParameters() {
+    if (_parsed) {
+      return params;
     }
+    byte[] body = getHttpRequestBodyAsBytes();
+    if (body == null) {
+      _parsed = true;
+      return params;
+    }
+    QueryStringDecoder decoder = new QueryStringDecoder("?" + new String(body));
+    params.putAll(decoder.parameters());
+    _parsed = true;
+    return params;
+  }
 }
